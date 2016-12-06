764660fc3446f5c251462efd7bfb71714dafa4e9
Sai Chandra <saichandraiit42@gmail.com>
2016-12-06 17:12:18 +0530

    Circular slider
    Circular slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,27 @@
-function defaults(){
-    var count=document.getElementsByClassName("slider_images").length;
-    for(i=0;i<count;i++){
+$(document).ready(function(){
+    var count=document.getElementsByClassName("slider_images").length-1;
+    for(i=0;i<=count;i++){
       document.getElementsByClassName("slider_images")[i].style.display='none';  
     }
     document.getElementsByClassName("slider_images")[0].style.display='block';
-    current_slide=1;
-};
-
+    current_slide=0;
+});
 
 function slide(n){
-   var count=document.getElementsByClassName("slider_images").length;     
-   current_slide=current_slide+n;   
-   for(i=1;i<=count;i++){
-      document.getElementsByClassName("slider_images")[i-1].style.display='none';  
+    var count=document.getElementsByClassName("slider_images").length-1;
+    current_slide=current_slide+n;
+    for(i=0;i<=count;i++){
+      document.getElementsByClassName("slider_images")[i].style.display='none';  
     }
-   if(current_slide>=1 && current_slide<=count){
-         document.getElementsByClassName("slider_images")[current_slide-1].style.display='block';                 
-   }
-   else{   
-        current_slide=1;
-        document.getElementsByClassName("slider_images")[0].style.display='block';         
-        console.log(current_slide); 
-  }  
-};
+    if(current_slide>=0 && current_slide<=count){
+        document.getElementsByClassName("slider_images")[current_slide].style.display='block';
+    }
+    else if(current_slide>count){
+        current_slide=0;
+        document.getElementsByClassName("slider_images")[0].style.display='block';
+    }
+    else{
+        current_slide=count;
+        document.getElementsByClassName("slider_images")[count].style.display='block';
+    }
+}
